b681ec85e4187843a2c72685f64ca9562c559867
Gregory <gregoryg97@hotmail.com>
2020-02-13 12:19:31 -0500

    classe saison opérateurs += -= == et les méthodes d acces

--- TP1-H20-Windows/TP1-H20/src/Saison.cpp
+++ TP1-H20-Windows/TP1-H20/src/Saison.cpp
@@ -2,18 +2,18 @@
 #include "Saison.h"
 
 // To do
-Saison::Saison():
-    // To do
-    numSaison_(0), 
-    nbEpisodesmax_(0)
+Saison::Saison()
+    : // To do
+    numSaison_(0)
+    , nbEpisodesmax_(0)
 {
 }
 
 // To do
-Saison::Saison(unsigned int numSaison, unsigned int nbEpisodemax):
-    // To do
-    numSaison_(numSaison), 
-    nbEpisodesmax_(nbEpisodemax)
+Saison::Saison(unsigned int numSaison, unsigned int nbEpisodemax)
+    : // To do
+    numSaison_(numSaison)
+    , nbEpisodesmax_(nbEpisodemax)
 {
 }
 
@@ -33,24 +33,46 @@ Saison::Saison(const Saison& saison)
 Saison::~Saison()
 {
     // To do
+    episodes_.clear();
 }
 
 // To do
 Saison& Saison::operator+=(std::unique_ptr<Episode> episode)
 {
     // To do
+    int indexEpisode = trouverIndexEpisode(episode->getNumEpisode);
+    if (indexEpisode != -1)
+    {
+        episodes_[indexEpisode] = std::move(episodes_[episodes_.size() - 1]);
+        episodes_.pop_back();
+    }
+    episodes_.push_back(std::make_unique<Episode>(episode));
+    sort(episodes_.begin(), episodes_.end(), Episode::SortByNumEpisode());
+    return *this;
 }
 
 // To do
 Saison& Saison::operator-=(unsigned int numEpisode)
 {
     // To do
+    int indexEpisode = trouverIndexEpisode(numEpisode);
+    if (indexEpisode != -1)
+    {
+        episodes_[indexEpisode] = std::move(episodes_[episodes_.size() - 1]);
+        episodes_.pop_back();
+    }
+    return *this;
 }
 
 // To do
 bool Saison::operator==(unsigned int numSaison)
 {
     // To do
+    if (numSaison_ == numSaison)
+    {
+        return true;
+    }
+    return false;
 }
 
 // To do
@@ -69,12 +91,13 @@ std::istream& operator>>(std::istream& is, Saison& saison)
 unsigned int Saison::getNumSaison() const
 {
     // To do
+    return numSaison_;
 }
 
 // To do
 size_t Saison::getNbEpisodes() const
-{
-   // To do
+{ // To do
+    return episodes_.size();
 }
 
 // To do
