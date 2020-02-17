037dea858c439681292db25fbc78515876a4e671
Olivier_Gamache <o.gamache2@gmail.com>
2020-02-17 13:03:03 -0500

    Terminer Classe Serie

--- TP1-H20-Windows/TP1-H20/src/Serie.cpp
+++ TP1-H20-Windows/TP1-H20/src/Serie.cpp
@@ -1,8 +1,11 @@
 // To do
+#include "Serie.h"
 
 // To do              Pointeur vers l'auteur du serie
 Serie::Serie(Auteur* auteur)
     // To do
+    : Media(auteur, TypeMedia::Serie)
+    , GestionnaireSaisons() // Pas sûr du constructeur de GestionnaireSaisons
 {
 }
 
@@ -10,24 +13,35 @@ Serie::Serie(Auteur* auteur)
 Serie::Serie(const std::string& nom, unsigned int anneeDeSortie, Genre genre, Pays pays,
              bool estRestreintParAge, Auteur* auteur)
     // To do
+    : Media(nom, anneeDeSortie, genre, pays, estRestreintParAge, auteur, TypeMedia::Serie)
+    , GestionnaireSaisons()
 {
 }
 
 // To do
 Serie::Serie(const Serie& serie)
     // To do
+    : Media(serie.nom_, serie.anneeDeSortie_, serie.genre_, serie.pays_, serie.estRestreintParAge_,
+            serie.auteur_, TypeMedia::Serie)
+    , GestionnaireSaisons()
 {
-    // To do
+    // To do (Je ne suis pas sûr s'il faut faire quelque chose de plus)
 }
 
 // To do
 std::ostream& Serie::afficher(std::ostream& os) const
 {
     // To do
+    for (int i = 0; i < saisons_.size(); i++)
+    {
+        os << saisons_[i];
+    }
+    return os;
 }
 
 // To do
 std::unique_ptr<Media> Serie::clone() const
 {
     // To do
+    return std::make_unique<Media>(*this); // Dans le .h, objet de type Media et non pas Serie...
 }
